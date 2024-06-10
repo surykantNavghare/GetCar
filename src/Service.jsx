d60a1aeba2b1d07e7ca4906adec554ed8c919163
RajatKohade <kohaderajat@gmail.com>
2024-06-10 12:23:52 +0530

    CSS

--- src/Service.jsx
+++ src/Service.jsx
@@ -8,49 +8,49 @@ let Service = () => {
         <div className="row">
             <div className="display-service">
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={`${process.env.PUBLIC_URL}/Images/service.png`} class="card-img-top" alt="..." style={{height:'200px',width:'200px',margin:'auto'}}/>
-                    <div class="card-body">
-                        <h5 class="card-title">Service</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <img src={`${process.env.PUBLIC_URL}/Images/service.png`} className="card-img-top" alt="..." style={{height:'200px',width:'200px',margin:'auto'}}/>
+                    <div className="card-body">
+                        <h5 className="card-title">Service</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                 </div>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={`${process.env.PUBLIC_URL}/Images/paint.png`} class="card-img-top" alt="..." style={{height:'200px',width:'200px',margin:'auto'}}/>
-                    <div class="card-body">
-                        <h5 class="card-title">Painting</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <img src={`${process.env.PUBLIC_URL}/Images/paint.png`} className="card-img-top" alt="..." style={{height:'200px',width:'200px',margin:'auto'}}/>
+                    <div className="card-body">
+                        <h5 className="card-title">Painting</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
                     </div>
                 </div>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={`${process.env.PUBLIC_URL}/Images/insurance.png`} class="card-img-top" alt="..." style={{height:'200px',width:'200px',margin:'auto'}}/>
-                    <div class="card-body">
-                        <h5 class="card-title">Insurance</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <img src={`${process.env.PUBLIC_URL}/Images/insurance.png`} className="card-img-top" alt="..." style={{height:'200px',width:'200px',margin:'auto'}}/>
+                    <div className="card-body">
+                        <h5 className="card-title">Insurance</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
                     </div>
                 </div>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={`${process.env.PUBLIC_URL}/Images/repair.png`} class="card-img-top" alt="..." style={{height:'200px',width:'200px',margin:'auto'}}/>
-                    <div class="card-body">
-                        <h5 class="card-title">Repair</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <img src={`${process.env.PUBLIC_URL}/Images/repair.png`} className="card-img-top" alt="..." style={{height:'200px',width:'200px',margin:'auto'}}/>
+                    <div className="card-body">
+                        <h5 className="card-title">Repair</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
                     </div>
                 </div>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={`${process.env.PUBLIC_URL}/Images/business.png`} class="card-img-top" alt="..." style={{height:'200px',width:'200px',margin:'auto'}}/>
+                    <img src={`${process.env.PUBLIC_URL}/Images/business.png`} className="card-img-top" alt="..." style={{height:'200px',width:'200px',margin:'auto'}}/>
                     <div class="card-body">
-                        <h5 class="card-title">Business</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 className="card-title">Business</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
                     </div>
                 </div>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={`${process.env.PUBLIC_URL}/Images/secure.png`} class="card-img-top" alt="..." style={{height:'200px',width:'200px',margin:'auto'}}/>
-                    <div class="card-body">
-                        <h5 class="card-title">Secure</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <img src={`${process.env.PUBLIC_URL}/Images/secure.png`} className="card-img-top" alt="..." style={{height:'200px',width:'200px',margin:'auto'}}/>
+                    <div className="card-body">
+                        <h5 className="card-title">Secure</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
                     </div>
                    
